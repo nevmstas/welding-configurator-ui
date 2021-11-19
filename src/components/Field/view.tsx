8125a346d7f9e26ade413e42e24cfed3396a06af
nevmstas <nevmyvakastas@gmail.com>
2021-11-20 03:51:56 +0600

    3 direction connections

--- src/components/Field/view.tsx
+++ src/components/Field/view.tsx
@@ -18,14 +18,39 @@ const Field = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const handleCellClick =
-    (i: number, j: number) => (direction: EDirection) => {
-      console.log(direction);
-      const newMatrix = [...matrix];
-      console.log(i, j);
-      newMatrix[i][j] = 1;
-      setMatrix(newMatrix);
-    };
+  const handleCellClick = (x: number, y: number) => (direction: EDirection) => {
+    console.log(x, y);
+    const newMatrix = [...matrix];
+    newMatrix[x][y] = 1;
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        switch (direction) {
+          case EDirection.LEFT:
+            if (i < x) {
+              newMatrix[i][y] = 1;
+            }
+            if (j < y) {
+              newMatrix[x][j] = 1;
+            }
+            break;
+          case EDirection.STRAIGHT:
+            newMatrix[x][j] = 1;
+            break;
+          case EDirection.RIGHT:
+            if (i <= x) {
+              newMatrix[i][y] = 1;
+            }
+            if (j >= y) {
+              newMatrix[x][j] = 1;
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    setMatrix(newMatrix);
+  };
 
   return (
     <div
